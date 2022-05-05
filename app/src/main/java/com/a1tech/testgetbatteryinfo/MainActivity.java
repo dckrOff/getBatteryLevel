6f3d3efb1fcd2ea171b5b1b382562354e0227cf7
dckrOff <azamatotaboev7621@gmail.com>
2022-05-05 18:11:33 +0500

    project is uploaded

---
+++ app/src/main/java/com/a1tech/testgetbatteryinfo/MainActivity.java
@@ -0,0 +1,51 @@
+package com.a1tech.testgetbatteryinfo;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Context context;
+    TextView textView;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.txt);
+        batteryLevel();
+    }
+
+    private void batteryLevel() {
+        BroadcastReceiver batteryLevelReceiver = new BroadcastReceiver() {
+            @SuppressLint("SetTextI18n")
+            public void onReceive(Context context, Intent intent) {
+                context.unregisterReceiver(this);
+                int rawlevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+                int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+                int level = -1;
+                if (rawlevel >= 0 && scale > 0) {
+                    level = (rawlevel * 100) / scale;
+                }
+                textView.setText("Battery Level Remaining: " + level + "%");
+                Toast.makeText(context, "Battery Level Remaining: " + level + "%", Toast.LENGTH_SHORT).show();
+            }
+        };
+        IntentFilter batteryLevelFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(batteryLevelReceiver, batteryLevelFilter);
+    }
+
+}
